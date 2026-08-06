7a12a2a29a1451d63ded401d58387ce21d577e97
antoinemalinur <antoine.malinur@gmail.com>
2026-08-05 20:10:59 -0400

    Initial Commit

---
+++ IAClient-UI/IAClient_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAClient_UIApp.swift
+//  IAClient-UI
+//
+//  Created by Antoine Malinur on 2026-08-05.
+//
+
+import SwiftUI
+
+@main
+struct IAClient_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
